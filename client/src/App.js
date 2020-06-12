a0256e78df98e38f68db7f49e31640adaefaaf3b
leonpsm <46592100+leonpsm@users.noreply.github.com>
2020-06-12 16:54:30 +0700

    Disable import css
    Temp solution, disable import css on client/src/index.js and client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
